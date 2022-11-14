0ba572dc144c222d78b48f7a3fb050fe15debc15
lijiaqi <lijq1103@gmail.com>
2022-11-14 11:32:54 +0800

    feat: apolloclient拆分

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client'
-import { setContext } from '@apollo/client/link/context'
-
-import { token } from '../local.config'
-
-const httpLink = createHttpLink({
-  uri: 'https://api.github.com/graphql',
-  credentials: 'same-origin'
-})
-const authLink = setContext((_, { headers }) => ({
-  headers: {
-    ...headers,
-    authorization: token ? `Bearer ${token}` : "",
-  }
-}))
-export const client = new ApolloClient({
-  ssrMode: true,
-  cache: new InMemoryCache(),
-  connectToDevTools: true,
-  link: authLink.concat(httpLink)
-})
+import { ApolloProvider } from '@apollo/client'
+import { client } from '../apollo/client'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <ApolloProvider client={client}>
