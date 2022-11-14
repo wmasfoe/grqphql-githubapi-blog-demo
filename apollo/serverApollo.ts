43a42f6e6b5939f28f98fcb9f5e8c48fd2db85c8
lijiaqi <lijq1103@gmail.com>
2022-11-14 14:57:13 +0800

    feat: github信息SSG

--- apollo/serverApollo.ts
+++
@@ -1,6 +0,0 @@
-import { gql } from '@apollo/client'
-import { client } from './client'
-
-async function query<T extends {[k: string]: string | number | boolean}>(query: any, params: T) {
-  const {} = await client.query({query: gql`${query}`})
-}
